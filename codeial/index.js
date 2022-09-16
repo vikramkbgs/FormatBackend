3a94c4ea5d84f599dda9aa02a669714eefd65564
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-09-16 15:30:22 +0530

    commenting && updating test console message

--- codeial/index.js
+++ codeial/index.js
@@ -8,13 +8,19 @@ const port = 8000;
 const app = express();
 
 
-app.listen(port, function(){
+app.use('/', require('./routes'));
+
+
+
+
+
+app.listen(port, function(err){
     if(err)
     {
         console.log(`Error in running: ${err}`)
         return;
     }
 
-    console.log('*****Server is running******');
+    console.log(`Server is running at port: ${port}`);
     return;
 })
