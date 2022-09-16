cae640604782605dd3fcc85ac0711a6dd7df18e7
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-09-16 13:26:56 +0530

    routing controller home_controllers.js

--- codeial/routes/index.js
+++ codeial/routes/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+
+
 const router = express.Router();
+const homeControlller = require('../controllers/home_controller');
+
 
 console.log('router loaded');
+
+router.get('/', homeControlller.home)
+
 
 module.exports = router;
