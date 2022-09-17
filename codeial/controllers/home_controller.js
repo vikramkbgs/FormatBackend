ee59823f6d901ef74144b61f716d5bda1905a7aa
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-09-17 12:05:38 +0530

    testing cookies

--- codeial/controllers/home_controller.js
+++ codeial/controllers/home_controller.js
@@ -1,6 +1,7 @@
 module.exports.home = function(req, res){
     //1. return res.end('<h1>Express is up for Codeial</h1>');
-
+    console.log(req.cookies);
+    res.cookie('name', 15);
     return res.render('home', {
         title : "home"
     });
