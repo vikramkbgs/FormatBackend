03470f8482d5faa2b691fb227364c998934efa8f
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-09-17 06:22:48 +0530

    set up in user controller for signin and sign up

--- codeial/controllers/users_controller.js
+++ codeial/controllers/users_controller.js
@@ -2,6 +2,19 @@ module.exports.profile = function(req, res){
   //1. return res.end('<h1>Express is up for Codeial</h1>');
 
   return res.render("user_profile", {
-    title: "profile",
+    title: "profile"
+  });
+}
+  //2. action render sign up page 
+module.exports.signUp = function(req, res){
+  return res.render('user_sign_up',{
+    title:'Codeial | Sign up'
+  });
+}
+
+//3. action render sign up page 
+module.exports.signIn = function(req, res){
+  return res.render('user_sign_in',{
+    title:'Codeial | Sign in'
   });
 }
