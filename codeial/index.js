38c4240a1ded616087df6d4644319762b7ba87fe
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-09-17 12:04:58 +0530

    adding  cookies-parser for cookies and rearranging modules

--- codeial/index.js
+++ codeial/index.js
@@ -1,34 +1,37 @@
 const express = require('express');
-
+const cookieParser = require('cookie-parser');
+const db = require("./config/mongoose");
+const expressLayouts = require("express-ejs-layouts");
 
 const port = 8000;
 
 
+
 const app = express();
+// use cookie parser for cookies
+app.use(cookieParser());
 
-const db = require('./config/mongoose');
-
-//use express layouts
-const expressLayouts = require("express-ejs-layouts");
-app.use(expressLayouts);
-
-
-// extracting style and script from subfile to layout 
-app.set('layout extractStyles', true);
-app.set("layout extractScripts", true);
-
-
-// use  express routes 
-app.use('/', require('./routes'));
-
+// use express.urlencoded for post request
+app.use(express.urlencoded());
 
 //set up the views engine ejs
 app.set('view engine', 'ejs');                    // debug extra space give you error
 app.set('views', './views');
 
-
 // use static file css js images
 app.use(express.static('assests'));
+
+//use express layouts
+app.use(expressLayouts);
+
+// use  express routes 
+app.use('/', require('./routes'));
+
+
+
+// extracting style and script from subfile to layout 
+app.set('layout extractStyles', true);
+app.set("layout extractScripts", true);
 
 
 
