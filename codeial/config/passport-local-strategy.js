eb036ecfa433754d3305c00a0c8e95601a4d9477
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-09-27 00:35:47 +0530

    updating config

--- codeial/config/passport-local-strategy.js
+++ codeial/config/passport-local-strategy.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 
-const localStrategy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 
 const User = require("../models/user");
 
