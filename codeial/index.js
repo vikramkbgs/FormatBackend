df414bbafc7dcbdaaefa44492020b2c40f18ff78
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-09-27 00:36:53 +0530

    fixing..

--- codeial/index.js
+++ codeial/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const db = require("./config/mongoose");
 const expressLayouts = require("express-ejs-layouts");
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
 
 const port = 8000;
 
@@ -23,6 +27,22 @@ app.use(express.static('assests'));
 
 //use express layouts
 app.use(expressLayouts);
+
+app.use(
+  session({
+    name: "codeial",
+    // TODO change the secret before deployment in production mode
+    secret: "blahsomething",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // use  express routes 
 app.use('/', require('./routes'));
--- codeial/routes/users.js
+++ codeial/routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require("passport");
 
 const router = express.Router();
 
@@ -14,6 +15,10 @@ router.get('/sign-up', usersController.signUp);
 
 router.post('/create', usersController.create);
 
-router.post("/create-session", usersController.createSession);
+router.post(
+  "/create-session",
+  passport.authenticate("local", { failureRedirect: "/users/sign-in" }),
+  usersController.createSession
+);
 
 module.exports = router;
