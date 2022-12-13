545c1a7f7d0c0ba55f6013e1738c08a9969659e0
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-12-13 16:40:40 +0530

    trying to fix assests

--- codeial/index.js
+++ codeial/index.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
 const db = require("./config/mongoose");
+app.use(express.static("./assets"));
+
 // used for session cookie
 const session = require("express-session");
 const passport = require("passport");
@@ -13,8 +15,6 @@ const MongoStore = require("connect-mongo")(session);
 app.use(express.urlencoded());
 
 app.use(cookieParser());
-
-app.use(express.static("./assets"));
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
