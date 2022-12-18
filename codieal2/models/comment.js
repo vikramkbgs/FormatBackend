f8849cef4150080a2a528507452921a835fcedf6
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-12-18 14:05:46 +0530

    removing extra commas

--- codieal2/models/comment.js
+++ codieal2/models/comment.js
@@ -3,16 +3,16 @@ const mongoose = require('mongoose');
 const commentSchema = new mongoose.Schema({
   content: {
     type: String,
-    required: true,
+    required: true
   },
   //comment belongs to a user
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+    ref: 'User'
   },
   post: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Post',
+    ref: 'Post'
   }
 }, {
     timestamps:true
